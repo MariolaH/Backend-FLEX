817302dfa5f6829cde5219f5ec915e834c3da647
MariolaH <mariola.hullings@student.awesomeinc.org>
2023-04-14 15:14:51 +0000

    set up

---
+++ flex/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
+
+# Create your models here.
---
+++ flex/urls.py
@@ -0,0 +1,23 @@
+from django.conf.urls.static import static
+from django.conf import settings
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
+from .views import *
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('user/signup/', UserCreate.as_view(), name="create_user"),
+    path('users/<int:pk>/', UserDetail.as_view(), name="get_user_details"),
+    path('user/login/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/obtain/', jwt_views.TokenObtainPairView.as_view(), name='token_create'),  # override sjwt stock token
+]
+
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
